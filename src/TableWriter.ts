5460cb427f0c90bdb3cb5ba2e382adc9180af2f9
Rohan Pethiyagoda <sarith21@gmail.com>
2017-10-28 12:47:13 -0700

    Add proper progress bar to table writer

--- src/TableWriter.ts
+++ src/TableWriter.ts
@@ -3,18 +3,39 @@ import { movies, ratings, table } from './Schema';
 import * as parse from 'csv-parse';
 import * as fs from 'fs';
 import { TupleBlock } from './TupleBlock';
+import * as ProgressBar from 'progress';
+
+import { exec } from 'child_process';
+
 const transform = require('stream-transform');
 
 export class TableWriter {
 
     private currentBlock: TupleBlock;
+    private progressBar: ProgressBar;
 
     constructor(private tupleSerializer: TupleSerializer){}
 
-    public createTable(table: table, sourcePath: string, destinationPath: string){
+    private countInputRows(sourcePath: string): Promise<number>{
+        return new Promise((resolve, reject) => {
 
-        this.currentBlock = new TupleBlock(this.tupleSerializer, table);
+            exec(`wc -l ${sourcePath}`, (err, stdout, stderr) => {
+                return err ? reject(err) : resolve(parseInt(stdout));
+            });
+        })
+    }
 
+    private setupProgressBar(rowCount: number){
+        this.progressBar = new ProgressBar(':percent [:bar] :rate/rps', {
+            complete: '=',
+            incomplete: ' ',
+            width: 100,
+            total: rowCount,
+            renderThrottle: 100
+        });
+    }
+
+    private writeTableToDisk(table: table, sourcePath: string, destinationPath: string){
         return new Promise((resolve, reject) => {
 
             const parser = parse({columns: true, auto_parse: true});
@@ -26,22 +47,36 @@ export class TableWriter {
             const transformer = transform((record: any, cb: any) => {
 
                 const added = this.currentBlock.addTuple(record);
+                this.progressBar.tick();
 
                 if(!added){
                     destination.write(this.currentBlock.getBytes());
                     this.currentBlock = new TupleBlock(this.tupleSerializer, table);
                     this.currentBlock.addTuple(record);
+                    this.progressBar.tick();
                 }
 
                 cb();
-                if(++linesParsed % 10000 === 0) process.stdout.write('.');
 
             }, {parallel: 1})
 
             input.pipe(parser).pipe(transformer).pipe(process.stdout);
-            input.on('end', () => setTimeout(resolve, 100) );
-
+            input.on('end', () => {
+                setTimeout(resolve, 100);
+                this.progressBar.tick = () => null;
+                this.progressBar.terminate();
+            });
         });
+    }
+
+    public createTable(table: table, sourcePath: string, destinationPath: string){
+
+        this.currentBlock = new TupleBlock(this.tupleSerializer, table);
+
+        return this.countInputRows(sourcePath)
+        .then((rowCount) => this.setupProgressBar(rowCount))
+        .then(() => this.writeTableToDisk(table, sourcePath, destinationPath))
+        .then(() => console.log('\n Done! \n'));
     }
 }
 
@@ -49,18 +84,17 @@ const x = new TableWriter(new TupleSerializer());
 
 // x.createTable(
 //     movies,
-//     '/Users/sarith21/Documents/code/toydb/data/originalData/movies.csv',
-//     '/Users/sarith21/Documents/code/toydb/data/movies.table'
+//     '/Users/rohanpethiyagoda/Documents/code/academics/bradfield/databases/toyDb/data/movies.csv',
+//     '/Users/rohanpethiyagoda/Documents/code/academics/bradfield/databases/toyDb/data/movies.table'
 // )
+// .then(() => process.exit());
 // .then(() => {
-//     console.log('finished movies');
-//     return x.createTable(
-//         ratings,
-//         '/Users/sarith21/Documents/code/toydb/data/originalData/ratings.csv',
-//         '/Users/sarith21/Documents/code/toydb/data/ratings.table'
-//     );
+    x.createTable(
+        ratings,
+        '/Users/rohanpethiyagoda/Documents/code/academics/bradfield/databases/toyDb/data/ratings.csv',
+        '/Users/rohanpethiyagoda/Documents/code/academics/bradfield/databases/toyDb/data/ratings.table'
+    )
 // })
-// .then(() => {
-//     console.log('finished ratings');
-//     setTimeout(() => process.exit(0), 200);
-// })
+.then(() => {
+    setTimeout(() => process.exit(0), 200);
+})
